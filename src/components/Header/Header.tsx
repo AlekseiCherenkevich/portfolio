577a5e5a8e691780ce249ab2f5473fb9bfa587c6
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2022-12-11 23:05:09 +0300

    create header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import style from './Header.module.css'
+import Nav from "./Nav/Nav";
+import styleContainer from "../common/styles/Container.module.css";
+
+const Header = () => {
+    return (
+        <div className={style.header}>
+            <div className={styleContainer.container}>
+                <Nav/>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
